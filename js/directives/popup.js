88221785fd3ccf8adbcfdf87c61be663d099a37a
lmsamiran <samiran.jana@learningmate.com>
2017-05-24 17:39:49 +0530

    change css font for darg

--- js/directives/popup.js
+++ js/directives/popup.js
@@ -4,7 +4,19 @@ angular.module(sortApp).directive('popUp', function() {
     scope: "true",
     templateUrl: 'video.html',
 	link:function(scope,attr,elem){
-		scope.showVideo=false;
+		scope.showVideo=true;
+		
+		scope.openModal=function(){
+			angular.element('#videoModal').modal("show");
+			if(angular.element('video').length){
+				angular.element('video')[0].play();
+			}
+		}
+		scope.closeModal=function(){
+			if(angular.element('video').length){
+				angular.element('video')[0].pause();
+			}
+		}
 		angular.element('#videoModal').on('hidden.bs.modal', function () {
 			if(angular.element('video').length){
 				angular.element('video')[0].pause();
